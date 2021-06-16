eb583c4ebc26b8b4229988778d3dc2fb5c6d2fab
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-06-16 16:31:53 -0400

    Add path to raw JSON files

--- lib/nomisma_xml_generator/coin_collection.rb
+++ lib/nomisma_xml_generator/coin_collection.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 module NomismaXmlGenerator
+  ##
+  # PUL's entire collection of coins, generated from a directory full of JSON
+  #  files pulled from the catalog.
   class CoinCollection
+    attr_reader :json_dir
+
+    def initialize(json_dir)
+      @json_dir = json_dir
+    end
   end
 end
--- spec/nomisma_xml_generator/coin_collection_spec.rb
+++ spec/nomisma_xml_generator/coin_collection_spec.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.describe NomismaXmlGenerator::CoinCollection do
-  let(:coin_collection) { described_class.new }
+  let(:coin_json_dir) { $fixture_path.to_s }
+  let(:coin_collection) { described_class.new(coin_json_dir) }
 
-  it 'is instantiated' do
+  it 'to have a path to coin JSON' do
     expect(coin_collection).instance_of? NomismaXmlGenerator::CoinCollection
+
+    expect(coin_collection.json_dir).to eq(coin_json_dir)
   end
 end
