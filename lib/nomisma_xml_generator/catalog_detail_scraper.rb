98b59df2ce52b7102fbcd7b0594d6e1a0b96588d
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-06-17 15:08:44 -0400

    Add output directory

--- lib/nomisma_xml_generator/catalog_detail_scraper.rb
+++ lib/nomisma_xml_generator/catalog_detail_scraper.rb
@@ -5,5 +5,10 @@ module NomismaXmlGenerator
   # Given a list of coin links (which can be provided by CatalogListScraper)
   #  collect all the raw JSON attached to each coin in the list
   class CatalogDetailScraper
+    attr_reader :output_dir
+
+    def initialize(output_dir: 'data/raw')
+      @output_dir = output_dir
+    end
   end
 end
--- spec/nomisma_xml_generator/catalog_detail_scraper_spec.rb
+++ spec/nomisma_xml_generator/catalog_detail_scraper_spec.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe NomismaXmlGenerator::CatalogDetailScraper do
-  let(:scraper) { described_class.new }
+  let(:output_dir) { "#{$output_dir}/raw" }
+  let(:scraper) { described_class.new(output_dir: output_dir) }
 
-  it 'can be instantiated' do
+  it 'has an output directory' do
     expect(scraper).to be_a_kind_of NomismaXmlGenerator::CatalogDetailScraper
+    expect(scraper.output_dir).to eq output_dir
   end
 end
